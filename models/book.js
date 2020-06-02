6ee828c114bb761d1269cbf2dc2ac413dc807cf6
Benoit Técher <techer.benoit2113@gmail.com>
2020-06-02 12:01:44 +0200

    ajout models/book

---
+++ models/book.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+    title : String,
+    author: String,
+    editor: String,
+    category: {
+        type: Schema.Types.ObjectId,
+        ref: 'Category'
+    },
+});
+
+module.exports = mongoose.model("Book", bookSchema);
