97e683eba336145ae27b00793800487cda5f51c6
souleymane Diallo <dialau85@yahoo.fr>
2020-06-02 10:34:07 +0200

    modifié

--- routes/borrowing.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-module.exports = (app) => {
-    const Borrowing = require("../models/borrowing");
-
-    router.post('/create', app.controllers.borrowing.create);
-    
-    return router;
-};
