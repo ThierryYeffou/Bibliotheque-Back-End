82525d1ef7204d9fba0f2837ced122672d843881
Benoit Técher <techer.benoit2113@gmail.com>
2020-06-02 09:39:54 +0200

    Remplissage initial de routes/index

---
+++ routes/index.js
@@ -0,0 +1,7 @@
+module.exports = (app) => {
+    
+
+    app.use("/borrowings", require("./borrowings")(app));
+    app.use("/subscribers", require("./subscribers")(app));
+    app.use("/books", require("./books")(app));
+}
