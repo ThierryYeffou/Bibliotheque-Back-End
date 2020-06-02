f0f07266c8ac2dbed878bbae090bfe83be0ba3cb
kdoranco <rk_doranco-cda@gmx.com>
2020-06-02 11:13:59 +0200

    Ajout de la route pour requete show

--- routes/borrowings.js
+++ routes/borrowings.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 module.exports = (app) => {
     const Borrowing = require("../models/borrowing");
 
+    router.post("/show", app.controllers.borrowings.show);
     router.post('/create', app.controllers.borrowings.create);
     router.post("/close", app.controllers.borrowings.close);
     
