7d7848ae2c7f32200688b428f67bb62a9a2eb922
Benoit Técher <techer.benoit2113@gmail.com>
2020-06-02 13:42:09 +0200

    suppression de const Borrowing dans routes/borrowings

--- routes/borrowings.js
+++ routes/borrowings.js
@@ -1,6 +1,5 @@
 const router = require("express").Router();
 module.exports = (app) => {
-    const Borrowing = require("../models/borrowing");
 
     router.post("/show", app.controllers.borrowings.show);
     router.post('/create', app.controllers.borrowings.create);
