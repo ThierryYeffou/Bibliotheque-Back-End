5f2c286cc5c24a898ef145e68387426d5d79d5ce
Benoit Técher <techer.benoit2113@gmail.com>
2020-06-02 10:37:58 +0200

    mise au pluriel de borrowing dans la route borrowings/create

--- routes/borrowings.js
+++ routes/borrowings.js
@@ -2,7 +2,8 @@ const router = require("express").Router();
 module.exports = (app) => {
     const Borrowing = require("../models/borrowing");
 
-    router.post('/create', app.controllers.borrowing.create);
+    router.post('/create', app.controllers.borrowings.create);
+    router.post("/close", app.controllers.borrowings.close);
     
     return router;
 };
