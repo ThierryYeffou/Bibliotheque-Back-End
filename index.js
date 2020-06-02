c980f911f484f2a6cf63ad99549829eb34c3baf5
Benoit Técher <techer.benoit2113@gmail.com>
2020-06-02 09:24:09 +0200

    ajout index.js racine

---
+++ index.js
@@ -0,0 +1,12 @@
+const app = require('express')();
+const port = 3000;
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+/*require("./controllers")(app);
+require("./routes")(app);
+require("./database")(app);
+require("./models")(app);*/
+
+app.listen(port, () => console.log(`Example app listening on port port!`))
