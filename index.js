c2d65823d1507f5fea02182b23005aea8a0c0d49
Thierry Yeffou <yeffouthierry@gmail.com>
2020-06-02 10:29:32 +0200

    test hello world

--- index.js
+++ index.js
@@ -8,5 +8,6 @@ app.use(bodyParser.json());
 require("./routes")(app);
 require("./database")(app);
 require("./models")(app);*/
+app.get('/',(req,res)=>res.send('hello world !'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
