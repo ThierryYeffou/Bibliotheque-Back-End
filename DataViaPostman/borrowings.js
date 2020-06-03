b32dd374850fd1b7aad00214697cd831bfb62890
Benoit Técher <techer.benoit2113@gmail.com>
2020-06-03 09:21:14 +0200

    DataViaPostman renseigné

--- DataViaPostman/borrowings.js
+++ DataViaPostman/borrowings.js
@@ -1,10 +1,32 @@
 // Copier la Partie Body des requetes  pour les partager via gitHub 
-
-[ 
-    {
-        "borrowingDate": "5 test: Life is good TestModeleV3",
-        "maxReturnDate": "2002-06-20",
-        "realReturnDate": "2005-06-20"
-    }
-
+Create :
+[
+	{
+		"subscriber": "5ed661e5063c2f3b54503acc",
+		"books": ["5ed665216d7f4834d8bc0562", "5ed666136d7f4834d8bc0565"],
+		"borrowingDate": "2003-06-20",
+		"maxReturnDate": "2005-06-20"
+	},
+	{
+		"subscriber": "5ed661e5063c2f3b54503ace",
+		"books": ["5ed666136d7f4834d8bc0567"],
+		"borrowingDate": "2004-06-20",
+		"maxReturnDate": "2006-06-20"
+	},
+	{
+		"subscriber": "5ed661e5063c2f3b54503acd",
+		"books": ["5ed666136d7f4834d8bc0566"],
+		"borrowingDate": "2003-07-20",
+		"maxReturnDate": "2005-07-20"
+	}
 ]
+
+Close :
+{
+	"id": "5ed64233764b9e0a18401634",
+	"realReturnDate": "2020-06-02"
+}
+Show :
+{
+	"id": "5ed63e6a6a1b8f04ac8bf341"
+}
--- DataViaPostman/subscribers.js
+++ DataViaPostman/subscribers.js
@@ -8,5 +8,36 @@
 	"adress": "chez moi",
 	"birthDate": "1986-08-21",
 	"birthTown": "Dijon"
-    }
+	},
+	{
+		"lastName": "Souleymane",
+		"firstName": "Diallo",
+		"mail": "testmail2",
+		"phone": "testphone2",
+		"adress": "chez moi2",
+		"birthDate": "1950-08-21",
+		"birthTown": "Alaska"
+	},
+	{
+		"lastName": "Rachid",
+		"firstName": "The King",
+		"mail": "testmail3",
+		"phone": "testphone3",
+		"adress": "chez moi3",
+		"birthDate": "1850-08-21",
+		"birthTown": "Alaska"
+	},
+	{
+		"lastName": "Thierry",
+		"firstName": "Le Fou",
+		"mail": "testmail4",
+		"phone": "testphone4",
+		"adress": "chez moi4",
+		"birthDate": "2050-08-21",
+		"birthTown": "Alaska"
+	}	
 ]
+Remove :
+{
+	"id": "5ed6612f063c2f3b54503acb"
+}
